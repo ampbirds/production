5dc633875716f3c011dba2473930e4d4b5b915c8
Amit Bhagat <anshubhagat201@gmail.com>
2021-08-15 14:12:25 +0530

    Prod

---
+++ precache-manifest.28700b4eed60aa73cc614603fb218a2a.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "81f6360864a50c8c9e0ae5793d6d4599",
+    "url": "./index.html"
+  },
+  {
+    "revision": "e6722cf2461b6aaf87ae",
+    "url": "./static/css/main.1b5ab283.chunk.css"
+  },
+  {
+    "revision": "7a6e722221a7c03031fa",
+    "url": "./static/js/2.80d8fe7d.chunk.js"
+  },
+  {
+    "revision": "e6722cf2461b6aaf87ae",
+    "url": "./static/js/main.ae558f95.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "098da4353077d9041b03aaf28be9670e",
+    "url": "./static/media/bike1.098da435.jpg"
+  },
+  {
+    "revision": "1a9f5a635354b02d4dd533d140884fc4",
+    "url": "./static/media/bike2.1a9f5a63.jpg"
+  },
+  {
+    "revision": "182991a39bb89f055c45a6de9b4da567",
+    "url": "./static/media/bike3.182991a3.jpg"
+  },
+  {
+    "revision": "e067dbc842ee9da2e232859ef1eb38aa",
+    "url": "./static/media/fea1.e067dbc8.jpg"
+  },
+  {
+    "revision": "7e268f12117ea34409f6de51fe66ed90",
+    "url": "./static/media/fea2.7e268f12.jpg"
+  },
+  {
+    "revision": "931beb29a41dba6e9955ef6c97816099",
+    "url": "./static/media/fea3.931beb29.jpg"
+  },
+  {
+    "revision": "420cdff146d113548a80edc3556fd246",
+    "url": "./static/media/fea4.420cdff1.jpg"
+  },
+  {
+    "revision": "8ef1e90e60aeb84a9f161f2035ad46ce",
+    "url": "./static/media/fea5.8ef1e90e.jpg"
+  },
+  {
+    "revision": "97f74a6080d00cacc28bd8857608743d",
+    "url": "./static/media/lekeIcon.97f74a60.png"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "./static/media/logo.ee7cd8ed.svg"
+  },
+  {
+    "revision": "43e7628369b34b1a73f233e04a0dbc3c",
+    "url": "./static/media/shape.43e76283.svg"
+  },
+  {
+    "revision": "d0d7dcc8d620e1f506f199c9f07de022",
+    "url": "./static/media/video1.d0d7dcc8.mp4"
+  },
+  {
+    "revision": "79c6185ec52b7e4fbc4396ba9deeeeea",
+    "url": "./static/media/video2.79c6185e.mp4"
+  }
+]);
